31205ccf67004dc62adb5f09aff97d86cb7dc28b
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-08-27 18:37:21 +0800

    Delete creator/builder directory

--- creator/builder/interface0/Item.java
+++
@@ -1,7 +0,0 @@
-package creator.builder.interface0;
-
-public interface Item {
-    String name();
-    Packing packing();
-    float price();
-}
--- creator/builder/interface0/Packing.java
+++
@@ -1,5 +0,0 @@
-package creator.builder.interface0;
-
-public interface Packing {
-    String pack();
-}
