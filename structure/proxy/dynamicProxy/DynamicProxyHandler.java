27252f5ceedfbed58527399a7d3b80e07ae08460
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-08-29 16:11:01 +0800

    Add files via upload

---
+++ structure/proxy/dynamicProxy/DynamicProxyHandler.java
@@ -0,0 +1,20 @@
+package structure.proxy.dynamicProxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class DynamicProxyHandler implements InvocationHandler {
+    private final Object object;
+
+    public DynamicProxyHandler(Object object){
+        this.object = object;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("考试要来了！赶紧考前突击一下！");
+        Object result = method.invoke(object,args);
+        System.out.println("考完试了！赶紧出去嗨皮一下！");
+        return result;
+    }
+}
---
+++ structure/proxy/dynamicProxy/Examer.java
@@ -0,0 +1,8 @@
+package structure.proxy.dynamicProxy;
+
+public interface Examer {
+    void chineseExam();
+    void mathExam();
+    void englishExam();
+    void scienceExam();
+}
